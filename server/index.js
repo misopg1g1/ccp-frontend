87dc5182638e850dd5998031fef77a0f11c4d4d2
Gerzon-MISO <g.bautistag@uniandes.edu.co>
2023-04-14 23:48:07 -0500

    Se configura el server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,5 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
 import express from 'express'
-import bodyParser from 'body-parser';
-import { createServer as createViteServer } from 'vite'
 import { login } from './routes/login/index.js'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 async function route(server) {
   login(server);
@@ -13,20 +7,8 @@ async function route(server) {
 
 async function createServer() {
   const app = express()
-
-  // Create Vite server in middleware mode and configure the app type as
-  // 'custom', disabling Vite's own HTML serving logic so parent server
-  // can take control
-  const vite = await createViteServer({
-    server: { middlewareMode: true },
-    appType: 'custom'
-  })
-
-  // Use vite's connect instance as middleware. If you use your own
-  // express router (express.Router()), you should use router.use
-  app.use(vite.middlewares)
-  app.use(bodyParser.json({ limit: '10mb', extended: true }));
-  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  app.use(express.json({ limit: '10mb', extended: true }));
+  app.use(express.urlencoded({ limit: '10mb', extended: true }));
   app.set('port', process.env.PORT || 3003);
   route(app);
   app.listen(app.get('port'), () => {
--- server/routes/login/index.js
+++ server/routes/login/index.js
@@ -1,17 +1,10 @@
 export const login = (server) => {
     console.log('server')
     server.post('/api/login', (req, res) => {
-        consumer.login(req.body)
-            .then(async r => {
-                let response = {...r};
-                if (response.error) {
-                    console.log('login', req, response.error);
-                }
-                res.json(response);
-            })
-            .catch(error => {
-                console.log('login', req, error);
-                res.status(400).send(error);
-            });           
+        res.json(
+            {
+                "Title": "Hola from server!"
+            }
+        );         
     });
 };
