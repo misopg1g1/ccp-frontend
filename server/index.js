feb9e8399059db1ad3624361b84762a01e59ed80
Gerzon-MISO <g.bautistag@uniandes.edu.co>
2023-04-14 22:34:17 -0500

    Add port and proxy config

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import express from 'express'
+import bodyParser from 'body-parser';
+import { createServer as createViteServer } from 'vite'
+import login from './routes/login/index.js'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+async function route(server) {
+  login(server);
+}
+
+async function createServer() {
+  const app = express()
+
+  // Create Vite server in middleware mode and configure the app type as
+  // 'custom', disabling Vite's own HTML serving logic so parent server
+  // can take control
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom'
+  })
+
+  // Use vite's connect instance as middleware. If you use your own
+  // express router (express.Router()), you should use router.use
+  app.use(vite.middlewares)
+  app.use(bodyParser.json({ limit: '10mb', extended: true }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  app.set('port', process.env.PORT || 3003);
+  route(app);
+  app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+  });
+}
+
+createServer()
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 /// <reference types="vite/client" />
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-
-  // expose .env.asd as process.env instead of import.meta since jest does not import meta yet
-  const envWithProcessPrefix = Object.entries(env).reduce(
-    (prev, [key, val]) => {
-      return {
-        ...prev,
-        ['process.env.' + key]: `"${val}"`,
-      }
+export default defineConfig({
+  server: {
+    port: 3000,
+    fs: {
+      allow: ['..'],
     },
-    {},
-  )
-
-  return {
-    define: envWithProcessPrefix,
-  }
+    proxy: {
+      '/api': 'http://localhost:3003',  
+    }
+  },
 })
