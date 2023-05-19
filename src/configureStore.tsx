892c7a66436ff170203b5771db5923081c0a525f
Gerzon-MISO <gerzon.bautista@dafiti.com.co>
2023-05-19 15:14:49 -0500

    Confiuración inicial

--- src/configureStore.tsx
+++ src/configureStore.tsx
@@ -1,9 +1,12 @@
 import { legacy_createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import axe from '@axe-core/react';
 import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 import persistedReducer from './reducers';
 import mySaga from './sagas';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
@@ -16,6 +19,7 @@ export default () => {
 
     if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
         middlewares.push(logger);
+        axe(React, ReactDOM, 1000);
     }
     store = legacy_createStore(persistedReducer(), compose(applyMiddleware(...middlewares)));
     const persistor = persistStore(store);
